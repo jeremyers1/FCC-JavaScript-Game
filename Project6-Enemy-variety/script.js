b20d54ffd1bc180f7079cf11513a4820e7a7f08c
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-03 11:06:18 -0700

    Add basic enemy test and fix eventListener bug

--- Project6-Enemy-variety/script.js
+++ Project6-Enemy-variety/script.js
@@ -1,6 +1,8 @@
 /** @type {HTMLCanvasElement} */ // Tell VSCode to suggest built-in Canvas Methods
 // wait for all sources to load before starting game
-document.addEventListener('load', function () {
+// he told me to use document.addEventListener('load'), but that didn't work as there is no Document: load event
+// https://developer.mozilla.org/en-US/docs/Web/API/Document
+window.addEventListener('load', function () {
 	const canvas = document.getElementById('canvas1');
 	const ctx = canvas.getContext('2d');
 	canvas.width = 500;
@@ -10,27 +12,47 @@ document.addEventListener('load', function () {
 	class Game {
 		constructor() {
 			this.enemies = [];
+			this.#addNewEnemy();
+			console.log(this.enemies);
 		}
-		update() {}
-		draw() {}
+		update() {
+			this.enemies.forEach(object => object.update());
+		}
+		draw() {
+			this.enemies.forEach(object => object.draw());
+		}
 		// private methods can only be called from within class
-		#addNewEnemy() {}
+		#addNewEnemy() {
+			this.enemies.push(new Enemy());
+		}
 	}
 
 	// basic class for all enemies
 	class Enemy {
-		constructor() {}
-		update() {}
-		draw() {}
+		constructor() {
+			this.x = 100;
+			this.y = 100;
+			this.width = 100;
+			this.height = 100;
+		}
+		update() {
+			this.x--;
+		}
+		draw() {
+			ctx.fillRect(this.x, this.y, this.width, this.height);
+		}
 	}
 
-  let lastTime = 0;
+	const game = new Game();
+	let lastTime = 0;
 	function animate(timeStamp) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const deltaTime = timeStamp - lastTime;
-    lastTime = timeStamp;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		const deltaTime = timeStamp - lastTime;
+		lastTime = timeStamp;
+		game.update();
+		game.draw();
 		// some code
 		requestAnimationFrame(animate);
-  }
-  animate(0);
+	}
+	animate(0);
 });
