381675026ada9b86a550704b5640897d5f6a6c13
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-03 12:37:29 -0700

    Add worm enemy type

--- Project6-Enemy-variety/script.js
+++ Project6-Enemy-variety/script.js
@@ -23,18 +23,21 @@ window.addEventListener('load', function () {
 				this.enemies = this.enemies.filter(object => !object.markedForDeletion);
 				this.#addNewEnemy();
 				this.enemyTimer = 0;
-				console.log(this.enemies);
 			} else {
 				this.enemyTimer += deltaTime;
 			}
-			this.enemies.forEach(object => object.update());
+			this.enemies.forEach(object => object.update(deltaTime));
 		}
 		draw() {
 			this.enemies.forEach(object => object.draw(this.ctx));
 		}
 		// private methods can only be called from within class
 		#addNewEnemy() {
-			this.enemies.push(new Enemy(this));
+			this.enemies.push(new Worm(this));
+			// to make higher-index enemies appear behind lower index enemies
+			this.enemies.sort(function (a, b) {
+				return a.y - b.y;
+			});
 		}
 	}
 
@@ -42,20 +45,46 @@ window.addEventListener('load', function () {
 	class Enemy {
 		constructor(game) {
 			this.game = game;
-			this.x = this.game.width;
-			this.y = Math.random() * this.game.height;
-			this.width = 100;
-			this.height = 100;
 			this.markedForDeletion = false;
 		}
-		update() {
-			this.x--;
+		update(deltaTime) {
+			this.x -= this.vx * deltaTime;
 			if (this.x < 0 - this.width) {
 				this.markedForDeletion = true;
 			}
 		}
 		draw(ctx) {
-			ctx.fillRect(this.x, this.y, this.width, this.height);
+			// ctx.fillRect(this.x, this.y, this.width, this.height);
+			// ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+			ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+		}
+	}
+
+	class Worm extends Enemy {
+		constructor(game) {
+			super(game); // runs parent class constructor
+			this.spriteWidth = 229;
+			this.spriteHeight = 171;
+			this.width = this.spriteWidth / 2;
+			this.height = this.spriteHeight / 2;
+			this.x = this.game.width;
+			this.y = Math.random() * this.game.height;
+			this.image = worm; // any html elment with an id is automatically added as a global variable by JS
+			this.vx = Math.random() * 0.1 + 0.1; // velocity on horizontal axis x
+		}
+	}
+
+	class Ghost extends Enemy {
+		constructor(game) {
+			super(game); // runs parent class constructor
+			this.spriteWidth = 261;
+			this.spriteHeight = 209;
+			this.width = this.spriteWidth / 2;
+			this.height = this.spriteHeight / 2;
+			this.x = this.game.width;
+			this.y = Math.random() * this.game.height;
+			this.image = ghost; 
+			this.vx = Math.random() * 0.2 + 0.1; 
 		}
 	}
 
