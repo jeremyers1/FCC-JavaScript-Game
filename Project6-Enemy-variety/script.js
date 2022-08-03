df8c17510a7d28db8dec75a742fcca3c83cf7a99
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-03 14:31:56 -0700

    Add animation from sprite sheets

--- Project6-Enemy-variety/script.js
+++ Project6-Enemy-variety/script.js
@@ -50,17 +50,33 @@ window.addEventListener('load', function () {
 		constructor(game) {
 			this.game = game;
 			this.markedForDeletion = false;
+			// animate enemies across sprite sheets
+			this.frameX = 0;
+			this.maxFrame = 5;
+			this.frameInterval = 100;
+			this.frameTimer = 0;
 		}
 		update(deltaTime) {
 			this.x -= this.vx * deltaTime;
 			if (this.x < 0 - this.width) {
 				this.markedForDeletion = true;
 			}
+			// animate enemies across sprite sheets
+			if (this.frameTimer > this.frameInterval) {
+				if (this.frameX < this.maxFrame) {
+					this.frameX++;
+				} else {
+					this.frameX = 0;
+				}
+				this.frameTimer += deltaTime;
+			} else {
+				this.frameTimer += deltaTime;
+			}
 		}
 		draw(ctx) {
 			// ctx.fillRect(this.x, this.y, this.width, this.height);
 			// ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-			ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+			ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
 		}
 	}
 
