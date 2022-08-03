774dd7ee1d37344d7c501d6a797f8fd028278671
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-03 10:42:13 -0700

    Add load eventListener and deltaTime so game runs same on all computers

--- Project6-Enemy-variety/script.js
+++ Project6-Enemy-variety/script.js
@@ -1,30 +1,36 @@
 /** @type {HTMLCanvasElement} */ // Tell VSCode to suggest built-in Canvas Methods
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 800;
+// wait for all sources to load before starting game
+document.addEventListener('load', function () {
+	const canvas = document.getElementById('canvas1');
+	const ctx = canvas.getContext('2d');
+	canvas.width = 500;
+	canvas.height = 800;
 
-// basic class for game movement and effects
-class Game {
-	constructor() {
-		this.enemies = [];
+	// basic class for game movement and effects
+	class Game {
+		constructor() {
+			this.enemies = [];
+		}
+		update() {}
+		draw() {}
+		// private methods can only be called from within class
+		#addNewEnemy() {}
 	}
-	update() {}
-	draw() {}
-	// private methods can only be called from within class
-	#addNewEnemy() {}
-}
 
-// basic class for all enemies
-class Enemy {
-	constructor() {}
-	update() {}
-	draw() {}
-}
+	// basic class for all enemies
+	class Enemy {
+		constructor() {}
+		update() {}
+		draw() {}
+	}
 
-function animate() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// some code
-	requestAnimationFrame(animate);
-}
-
+  let lastTime = 0;
+	function animate(timeStamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+		// some code
+		requestAnimationFrame(animate);
+  }
+  animate(0);
+});
