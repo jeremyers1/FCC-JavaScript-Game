7fa15e8853f4b4a68d0c49e90fcfe7dbb17dfc37
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-16 15:52:45 -0700

    Add player movement handler

--- Project7-Side-scroller-mobile/script.js
+++ Project7-Side-scroller-mobile/script.js
@@ -2,11 +2,33 @@ window.addEventListener('load', function () {
 	/** @type {HTMLCanvasElement} */ // for some reason, only works inside event listener
 	const canvas = document.getElementById('canvas1');
 	const ctx = canvas.getContext('2d');
-
 	canvas.width = 800;
 	canvas.height = 720;
 
-	class InputHandler {}
+	class InputHandler {
+		constructor() {
+			this.keys = [];
+			window.addEventListener('keydown', e => {
+				// prettier-ignore
+				if ((	e.key === 'ArrowDown' ||
+							e.key === 'ArrowUp' ||
+							e.key === 'ArrowLeft' ||
+							e.key === 'ArrowRight')
+							&& this.keys.indexOf(e.key) === -1) {
+					this.keys.push(e.key);
+				}
+			});
+			window.addEventListener('keyup', e => {
+				// prettier-ignore
+				if (	e.key === 'ArrowDown' ||
+							e.key === 'ArrowUp' ||
+							e.key === 'ArrowLeft' ||
+							e.key === 'ArrowRight'){
+					this.keys.splice(this.keys.indexOf(e.key), 1);
+				}
+			});
+		}
+	}
 
 	class Player {}
 
@@ -17,6 +39,8 @@ window.addEventListener('load', function () {
 	function handleEnemies() {}
 
 	function displayStatusText() {}
+
+	const input = new InputHandler();
 
 	function animate() {}
 });
