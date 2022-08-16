5ff17a105a4a94ad929ce0700e9b44d13fb39f85
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-16 16:12:56 -0700

    Add white rectangle horizontal movement

--- Project7-Side-scroller-mobile/script.js
+++ Project7-Side-scroller-mobile/script.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function () {
-	/** @type {HTMLCanvasElement} */ // for some reason, only works inside event listener
 	const canvas = document.getElementById('canvas1');
 	const ctx = canvas.getContext('2d');
 	canvas.width = 800;
@@ -30,7 +29,23 @@ window.addEventListener('load', function () {
 		}
 	}
 
-	class Player {}
+	class Player {
+		constructor(gameWidth, gameHeight) {
+			this.gameWidth = gameWidth;
+			this.gameHeight = gameHeight;
+			this.width = 200;
+			this.height = 200;
+			this.x = 0;
+			this.y = this.gameHeight - this.height;
+		}
+		draw(context) {
+			context.fillStyle = 'white';
+			context.fillRect(this.x, this.y, this.width, this.height);
+		}
+		update() {
+			this.x++;
+		}
+	}
 
 	class Background {}
 
@@ -41,6 +56,14 @@ window.addEventListener('load', function () {
 	function displayStatusText() {}
 
 	const input = new InputHandler();
+	const player = new Player(canvas.width, canvas.height);
 
-	function animate() {}
+	function animate() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		player.draw(ctx);
+		player.update();
+		requestAnimationFrame(animate);
+
+	}
+	animate();
 });
