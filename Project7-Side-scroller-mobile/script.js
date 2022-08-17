3f3a7fc1c70d1395e514ee14475f99827b0a839d
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-17 15:13:18 -0700

    Resize and reposition colission hit boxes

--- Project7-Side-scroller-mobile/script.js
+++ Project7-Side-scroller-mobile/script.js
@@ -79,15 +79,20 @@ window.addEventListener('load', function () {
 		}
 
 		draw(context) {
+			/* 			context.lineWidth = 5;
+			context.strokeStyle = 'white';
+			context.beginPath();
+			context.arc(this.x + this.width / 2, this.y + this.height / 2 + 20, this.width / 3, 0, Math.PI * 2);
+			context.stroke(); */
 			context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 		}
 		update(input, deltaTime, enemies) {
 			// collision detection
 			enemies.forEach(enemy => {
-				const dx = enemy.x + enemy.width / 2 - (this.x + this.width / 2);
-				const dy = enemy.y + enemy.height / 2 - (this.y + this.height / 2);
+				const dx = enemy.x + enemy.width / 2 - 20 - (this.x + this.width / 2);
+				const dy = enemy.y + enemy.height / 2 - (this.y + this.height / 2 + 20);
 				const distance = Math.sqrt(dx * dx + dy * dy);
-				if (distance < enemy.width / 2 + this.width / 2) {
+				if (distance < enemy.width / 3 + this.width / 3) {
 					gameOver = true;
 				}
 			});
@@ -184,6 +189,11 @@ window.addEventListener('load', function () {
 			this.markedForDeletion = false;
 		}
 		draw(context) {
+			/* 			context.lineWidth = 5;
+			context.strokeStyle = 'white';
+			context.beginPath();
+			context.arc(this.x + this.width / 2 - 20, this.y + this.height / 2, this.width / 3, 0, Math.PI * 2);
+			context.stroke(); */
 			context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
 		}
 		update(deltaTime) {
@@ -273,7 +283,7 @@ window.addEventListener('load', function () {
 		lastTime = timeStamp;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		background.draw(ctx);
-		//background.update();
+		background.update();
 		player.draw(ctx);
 		player.update(input, deltaTime, enemies);
 		handleEnemies(deltaTime);
