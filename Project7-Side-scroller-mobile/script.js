1bf6e1b48c22bc8fd51e19494dfe57354e9e04df
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-17 11:24:27 -0700

    Add basic enemy collision

--- Project7-Side-scroller-mobile/script.js
+++ Project7-Side-scroller-mobile/script.js
@@ -4,6 +4,8 @@ window.addEventListener('load', function () {
 	canvas.width = 800;
 	canvas.height = 720;
 	let enemies = [];
+	let score = 0;
+	let gameOver = false;
 
 	class InputHandler {
 		constructor() {
@@ -52,15 +54,36 @@ window.addEventListener('load', function () {
 		draw(context) {
 			//context.fillStyle = 'white';
 			//context.fillRect(this.x, this.y, this.width, this.height);
+			context.strokeStyle = 'white';
+			context.strokeRect(this.x, this.y, this.width, this.height);
+			context.beginPath();
+			context.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2);
+			context.stroke();
+			// collision area
+			context.strokeStyle = 'blue';
+			context.beginPath();
+			context.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2);
+			context.stroke();
 			context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 		}
-		update(input, deltaTime) {
+		update(input, deltaTime, enemies) {
+			// collision detection
+			enemies.forEach(enemy => {
+				const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
+				const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
+				const distance = Math.sqrt(dx * dx + dy * dy);
+				if (distance < enemy.width / 2 + this.width / 2) {
+					gameOver = true;
+				}
+			});
 			// sprite animation
 			if (this.frameTimer > this.frameInterval) {
 				if (this.frameX >= this.maxFrame) {
 					this.frameX = 0;
-				} else { this.frameX++; }
-				this.frameTimer = 0
+				} else {
+					this.frameX++;
+				}
+				this.frameTimer = 0;
 			} else {
 				this.frameTimer += deltaTime;
 			}
@@ -139,8 +162,19 @@ window.addEventListener('load', function () {
 			this.frameTimer = 0;
 			this.frameInterval = 1000 / this.fps;
 			this.speed = 8;
+			this.markedForDeletion = false;
 		}
 		draw(context) {
+			context.strokeStyle = 'white';
+			context.strokeRect(this.x, this.y, this.width, this.height);
+			context.beginPath();
+			context.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2);
+			context.stroke();
+			// collision area
+			context.strokeStyle = 'blue';
+			context.beginPath();
+			context.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2);
+			context.stroke();
 			context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
 		}
 		update(deltaTime) {
@@ -155,12 +189,18 @@ window.addEventListener('load', function () {
 				this.frameTimer += deltaTime;
 			}
 			this.x -= this.speed;
+			if (this.x < 0 - this.width) {
+				this.markedForDeletion = true;
+				score++;
+			}
 		}
 	}
 
 	function handleEnemies(deltaTime) {
 		if (enemyTimer > enemyInterval + randomEnemyInterval) {
 			enemies.push(new Enemy(canvas.width, canvas.height));
+			console.log(enemies);
+			randomEnemyInterval = Math.random() * 1000 + 500;
 			enemyTimer = 0;
 		} else {
 			enemyTimer += deltaTime;
@@ -169,9 +209,23 @@ window.addEventListener('load', function () {
 			enemy.draw(ctx);
 			enemy.update(deltaTime);
 		});
+		enemies = enemies.filter(enemy => !enemy.markedForDeletion);
 	}
 
-	function displayStatusText() {}
+	function displayStatusText(context) {
+		context.font = '40px Helvetica';
+		context.fillStyle = 'black';
+		context.fillText('Score: ' + score, 20, 50);
+		context.fillStyle = 'white';
+		context.fillText('Score: ' + score, 22, 52);
+		if (gameOver) {
+			context.textAlign = 'center';
+			context.fillStyle = 'black';
+			context.fillText('GAME OVER, try again!', canvas.width / 2, 200);
+			context.fillStyle = 'white';
+			context.fillText('GAME OVER, try again!', canvas.width / 2 + 2, 202);
+		}
+	}
 
 	const input = new InputHandler();
 	const player = new Player(canvas.width, canvas.height);
@@ -189,9 +243,10 @@ window.addEventListener('load', function () {
 		background.draw(ctx);
 		//background.update();
 		player.draw(ctx);
-		player.update(input, deltaTime);
+		player.update(input, deltaTime, enemies);
 		handleEnemies(deltaTime);
-		requestAnimationFrame(animate);
+		displayStatusText(ctx);
+		if (!gameOver) requestAnimationFrame(animate);
 	}
 	animate(0);
 });
