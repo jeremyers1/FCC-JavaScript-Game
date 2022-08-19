94eff78501d7ae8286cf5dd528d331d9d425d793
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-19 09:22:49 -0700

    Add additional player states

--- Project8-State-management/utils.js
+++ Project8-State-management/utils.js
@@ -1,4 +1,5 @@
-export function drawStatusText(context, input) {
-	context.font = '30px Helvetica';
-	context.fillText('Last Input: ' + input.lastKey, 10, 50);
+export function drawStatusText(context, input, player) {
+	context.font = '28px Helvetica';
+	context.fillText('Last Input: ' + input.lastKey, 20, 50);
+	context.fillText('Active State: ' + player.currentState.state, 20, 90);
 }
