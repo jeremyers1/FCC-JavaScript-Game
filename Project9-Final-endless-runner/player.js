e3d4eb3a89c3a1d2cc09b03fa664beef3085fd46
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-20 10:53:07 -0700

    Add jumping

--- Project9-Final-endless-runner/player.js
+++ Project9-Final-endless-runner/player.js
@@ -8,6 +8,8 @@ export class Player {
 		this.image = player; // JS creates references in global namespace to all elements with IDs
 		this.speed = 0;
 		this.maxSpeed = 10;
+		this.vy = 0;
+		this.gravity = 1;
 	}
 	update(input) {
 		// horizontal movement
@@ -15,10 +17,18 @@ export class Player {
 		if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
 		else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
 		else this.speed = 0;
-		if (this.x < 0) this.x = 0; 
+		if (this.x < 0) this.x = 0;
 		if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+		// vertical movement
+		if (input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
+		this.y += this.vy;
+		if (!this.onGround()) this.vy += this.gravity;
+		else this.vy = 0;
 	}
 	draw(context) {
 		context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+	}
+	onGround() {
+		return this.y >= this.game.height - this.height;
 	}
 }
