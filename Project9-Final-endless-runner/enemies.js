8c9a305abeaa709d6f678518c5f19d326e8c1ee1
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-20 17:50:23 -0700

    Add climbing enemy

--- Project9-Final-endless-runner/enemies.js
+++ Project9-Final-endless-runner/enemies.js
@@ -66,7 +66,32 @@ export class GroundEnemy extends Enemy {
 	}
 }
 
-export class ClimbingEnemy extends Enemy {}
+export class ClimbingEnemy extends Enemy {
+	constructor(game) {
+		super();
+		this.game = game;
+		this.width = 120;
+		this.height = 144;
+		this.x = this.game.width;
+		this.y = Math.random() * this.game.height * 0.5;
+		this.image = spiderBig;
+		this.speedX = 0;
+		this.speedY = Math.random() > 0.5 ? 1 : -1;
+		this.maxFrame = 5;
+	}
+	update(deltaTime) {
+		super.update(deltaTime);
+		if (this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+		if (this.y < -this.height) this.markedForDeletion = true;
+	}
+	draw(context) {
+		super.draw(context);
+		context.beginPath();
+		context.moveTo(this.x + this.width/2, 0);
+		context.lineTo(this.x + this.width/2, this.y + 50);
+		context.stroke();
+	}
+}
 
 // could do the following: but would need to have width, height, x, y, etc, declared separately on all
 class Bird extends FlyingEnemy {}
